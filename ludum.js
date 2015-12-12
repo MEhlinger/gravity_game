a5afeb5045e574c07ea26d17ca2284df04131418
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-12-12 14:41:13 -0500

    Add gravity logic

--- ludum.js
+++ ludum.js
@@ -10,10 +10,10 @@ canvas.width = 960;
 canvas.height = 580;
 document.body.appendChild(canvas);
 
-bgColor = BLACK = "#000000";
-// MAX_BIOMASS = 100;
-// MAX_SPEED = 50;
-// MAX_MASS = 100
+const bgColor = BLACK = "#000000";
+const MAX_BIOMASS = 100;
+const MAX_SPEED = 50;
+const MAX_MASS = 100
 
 var keysPressed = {};
 addEventListener("keydown", function(e) {keysPressed[e.keyCode] = true;}, false);
@@ -27,6 +27,13 @@ var pc = {
 	image : "/assets/images/pc.png"
 };
 
+var testMeteor = {
+	position : {x: 0, y: 0},
+	speed : {x: -5, y: 0},
+	mass : 5,
+	id : 0
+}
+
 var pcReady = false;
 var pcImage = new Image();
 pcImage.src = "assets/images/pc.png";
@@ -36,18 +43,66 @@ pcImage.onload = function() {
 
 var map = {
 	width : 1920,
-	height : 1060
+	height : 1060,
+	massiveObjects : [pc, testMeteor]
 };
 
 var setup = function() {
 	pc.position.x = 10;
 	pc.position.y = 10;
-	pc.mass = 10;
+	testMeteor.position = {x:pc.position.x + canvas.width / 2, y: pc.position.y + 50};
+	pc.mass = 32;
 	pc.biomass = 1;
 };
 
+var isOnScreen = function(objectInSpaceX, objectInSpaceY) {
+	if (
+		(objectInSpaceX <= pc.x + canvas.width / 2)
+		&& (objectInSpaceX >= pc.x - canvas.width / 2)
+		&& (objectInSpaceY <= pc.y + canvas.height / 2)
+		&& (objectInSpaceY >= pc.y - canvas.height / 2)
+		) {
+		return true;	
+	}
+	return false;
+};
+
+var orderObjectsByMass = function(obj1, obj2) {
+	// larger object is first
+	if (obj1.mass > obj2.mass) {
+		return [obj1, obj2];
+	}
+	return [obj2, obj1];
+}
+
+var gravityRadius = function(object) {
+	return object.mass * 3;
+}
+
+var exertGravityOnMassiveObjects = function() {
+	for (i = 0; i < map.massiveObjects.length; i++){
+		for (j = i+1; j < map.massiveObjects.length; j++) {
+			orderedObjects = orderObjectsByMass(map.massiveObjects[i], map.massiveObjects[j]);
+			xDiff = orderedObjects[0].position.x - orderedObjects[1].position.x;
+			yDiff = orderedObjects[0].position.y - orderedObjects[1].position.y;
+			if (xDiff <= gravityRadius(orderedObjects[0]) && xDiff > 0) {
+				orderedObjects[1].speed.x += orderedObjects[0].mass / orderedObjects[1].mass;
+			} else if (xDiff <= gravityRadius(orderedObjects[0]) && xDiff < 0) {
+				orderedObjects[1].speed.x -= orderedObjects[0].mass / orderedObjects[1].mass;
+			}
+			if (yDiff <= gravityRadius(orderedObjects[0]) && yDiff > 0) {
+				orderedObjects[1].speed.y += orderedObjects[0].mass / orderedObjects[1].mass;
+			} else if (yDiff <= gravityRadius(orderedObjects[0]) && yDiff < 0) {
+				orderedObjects[1].speed.y -= orderedObjects[0].mass / orderedObjects[1].mass;
+			}
+		}
+	}
+}
+
 var update = function(modifier) {
-	//Handle key presses
+	// Handle key presses
+	// Handle gravity and speed logic
+	exertGravityOnMassiveObjects();
 };
 
 var clearAndRedrawBackground = function() {
@@ -56,15 +111,15 @@ var clearAndRedrawBackground = function() {
 	context.fillRect(0, 0, canvas.width, canvas.height);
 };
 
-// var drawCircle(centerX, centerY, radius, color) {
-// 	context.beginPath();
-// 	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-// 	context.fillStyle = 'green';
-// 	context.fill();
-// 	context.lineWidth = 5;
-// 	context.strokeStyle = '#003300';
-// 	context.stroke();
-// }
+var drawCircle = function(centerX, centerY, radius, color) {
+	context.beginPath();
+	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+	context.fillStyle = color;
+	context.fill();
+	context.lineWidth = 5;
+	context.strokeStyle = color;
+	context.stroke();
+};
 
 var render = function() {
 	clearAndRedrawBackground();
@@ -72,8 +127,10 @@ var render = function() {
 		context.drawImage(pcImage,
 			canvas.width / 2 - pc.mass / 2, 
 			canvas.height / 2 - pc.mass / 2,
-			32, 32);
+			pc.mass, pc.mass);
 	}
+
+	drawCircle(testMeteor.position.x, testMeteor.position.y, testMeteor.mass / 2, "#FFFFFF");
 };
 
 var main = function() {
@@ -95,7 +152,6 @@ var supportRequestAnimationFrameCrossBrowser = function() {
 // THE GAME //
 /////////////
 supportRequestAnimationFrameCrossBrowser();
-
 var lastUpdate = Date.now();
 setup(); 
 main();
